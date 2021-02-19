beac97a48c2749f6a38186ac59ce1b6afbf1c181
WhiteNoise09 <whitenoise09@free.fr>
2021-02-19 02:39:56 +0100

    on DOM content loaded

--- src/videoStream.js
+++ src/videoStream.js
@@ -1,30 +1,32 @@
-const videoElement = document.querySelector('#cam-stream');
+window.addEventListener('DOMContentLoaded', () => {
+	const videoElement = document.querySelector('#cam-stream');
 
-if(!('BarcodeDetector' in window)) {
-	alert('BarcodeDetector api is not supported on this browser.')
-} else {
-	var barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] })
-}
-
-(async function() {
-	let media = null;
+	if(!('BarcodeDetector' in window)) {
+		alert('BarcodeDetector api is not supported on this browser.')
+	} else {
+		var barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] })
+	}
 	
-	try {
-	    media = await navigator.mediaDevices.getUserMedia({audio: false, video: { facingMode: 'environment' } });
-	}
-	catch(e) {
-		document.querySelector('body').textContent = e;
-	}
-
-	videoElement.srcObject = media;
-	videoElement.play();
-
-	document.querySelector('#scan-button').addEventListener('click', () => {
-		BarcodeDetector.detect(videoElement).then(datas => {
-			window.location = datas.rawValue;
-		});
-	})
-})();
+	(async function() {
+		let media = null;
+		
+		try {
+		    media = await navigator.mediaDevices.getUserMedia({audio: false, video: { facingMode: 'environment' } });
+		}
+		catch(e) {
+			document.querySelector('body').textContent = e;
+		}
+	
+		videoElement.srcObject = media;
+		videoElement.play();
+	
+		document.querySelector('#scan-button').addEventListener('click', () => {
+			BarcodeDetector.detect(videoElement).then(datas => {
+				window.location = datas.rawValue;
+			});
+		})
+	})();
+});
 
 /*
 function getCurrentImage(video) {
